a5a8196e4a932aaf2430a89f974a8e450f3705b0
richardverheyen <richard.verheyen@gmail.com>
2017-11-12 12:59:31 -0800

    update(pie-chart) build svg pie chart from data

--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -1,37 +1,44 @@
 import React, { Component } from 'react';
 
 class PieChart extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      data: [
+        {
+          value: 15,
+          isBig: false,
+          name: 'tea',
+          attributes: [[0, -100], [70.7, 29.3]]
+        },
+        {
+          value: 85,
+          isBig: true,
+          name: 'coffee',
+          attributes: [[70.7, -70.7], [-70.7, -29.3]]
+        }
+      ]
+    };
+  }
+
+  generateSvgPoints(slice){
+    let centre = 'M 100 100 ';
+    let start = `l ${slice.attributes[0][0]} ${slice.attributes[0][1]} `;
+    let curve = `a 100 100, 0, ${slice.isBig ? 1 : 0}, 1, ${slice.attributes[1][0]} ${slice.attributes[1][1]} `;
+    let closure = 'Z';
+    return centre + start + curve + closure;
+  }
+
   render() {
     return (
       <div id="landing">
         <svg width="200" height="200">
-          <title>Pie Chart</title>
-         <g>
-            <path d="
-            M 100 100
-            l 0 -100
-            a 100 100, 0, 0, 1, 70.7 29.3
-            Z"
-            fill="red" />
-            <path d="
-            M 100 100
-            l 70.7 -70.7
-            A 100 100, 0, 0, 1, 100 200
-            Z"
-            fill="green" />
-            <path d="
-            M 100 100
-            l 0 100
-            a 100 100, 0, 0, 1, -100 -100
-            Z"
-            fill="blue" />
-            <path d="
-            M 100 100
-            L 0 100
-            A 100 100, 0, 0, 1, 100 0
-            Z"
-            fill="yellow" />
-         </g>
+          <path
+            d={this.generateSvgPoints(this.state.data[0])} />
+          <path
+            d={this.generateSvgPoints(this.state.data[1])} />
         </svg>
       </div>
     );
